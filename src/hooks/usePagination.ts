4b87c7560f750dc9c347de7c205d97d8149dcae7
Yosef <mercytv932@gmail.com>
2026-09-09 14:01:38 -0400

    useState

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -4,4 +4,7 @@ export function usePagination(
   totalItems: number,
   itemsPerPage: number = 10,
   intialPage: number = 1,
-) {}
+) {
+  const [currentPage, setCurrentPage] = useState(intialPage);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+}
