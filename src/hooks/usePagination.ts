d9b2b5451e70b8048872cf4aabcbd2c9f15ec29c
Yosef <mercytv932@gmail.com>
2026-09-09 14:16:28 -0400

    Next/Previous page funcytions done

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -3,13 +3,25 @@ import { useState } from "react";
 export function usePagination(
   totalItems: number,
   itemsPerPage: number = 10,
-  intialPage: number = 1,
+  initialPage: number = 1,
 ) {
-  const [currentPage, setCurrentPage] = useState(intialPage);
+  const [currentPage, setCurrentPage] = useState(initialPage);
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
   const itemsOnCurrentPage = endIndex - startIndex;
   const canNextPage = currentPage < totalPages;
   const canPreviousPage = currentPage > 1;
+
+  function nextPage() {
+    if (canNextPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function previousPage() {
+    if (canPreviousPage) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
 }
