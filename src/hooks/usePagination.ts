5ec5cd6441510a997a09960b5ad19b11e34d3f2e
Yosef <mercytv932@gmail.com>
2026-09-09 14:09:03 -0400

    done setting useStates

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -7,4 +7,9 @@ export function usePagination(
 ) {
   const [currentPage, setCurrentPage] = useState(intialPage);
   const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
+  const itemsOnCurrentPage = endIndex - startIndex;
+  const canNextPage = currentPage < totalPages;
+  const canPreviousPage = currentPage > 1;
 }
