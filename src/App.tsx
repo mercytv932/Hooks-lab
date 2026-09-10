6ae96ffa2f420f8e322c5f8a0cae0240271831b3
Yosef <mercytv932@gmail.com>
2026-09-09 22:08:08 -0400

    done with DebounceSearchDemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import PaginationDemo from "./components/PaginationDemo";
+import DebounceSearchDemo from "./components/DebounceSearchDemo";
 
 function App() {
   return (
     <div>
       <PaginationDemo />
+      <DebounceSearchDemo />
     </div>
   );
 }
--- src/components/DebounceSearchDemo.tsx
+++ src/components/DebounceSearchDemo.tsx
@@ -3,6 +3,7 @@ import { useDebounce } from "../hooks/useDebounce";
 
 export default function DebounceSearchDemo() {
   const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search);
 
   return (
     <div>
@@ -14,6 +15,9 @@ export default function DebounceSearchDemo() {
         onChange={(e) => setSearch(e.target.value)}
         placeholder="Search..."
       />
+
+      <p>Current value: {search}</p>
+      <p>Debounced value: {debouncedSearch}</p>
     </div>
   );
 }
